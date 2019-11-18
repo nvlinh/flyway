5582561987ae7b8048620b3e4079cef6584972b9
nvlinh <linh@dactech.vn>
2019-11-18 11:35:41 +0700

    first commit

---
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -0,0 +1,9 @@
+create table A
+(
+    column1                 varchar(60)                          not null,
+    column2                varchar(60)                          not null,
+    column3                 varchar(60)                          not null,
+    column4               varchar(256)                         null,
+    primary key (column1)
+)
+    collate = utf8mb4_unicode_ci;
